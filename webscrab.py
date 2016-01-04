bec9cb09824faaef614815776b7f41d5d73594d8
lucyking <joehisaishi1943@gmail.com>
2016-01-04 18:12:50 +0800

    +python parameter parse: [--include=demo] .etc

--- webscrab.py
+++ webscrab.py
@@ -1,19 +1,34 @@
 # coding=utf-8
 import urllib, urllib2
-import sys, os,re, threading,platform,subprocess
+import sys, os, re, getopt
+import threading, platform, subprocess
 from time import ctime, sleep
 from HTMLParser import HTMLParser
 
 
 class MyParser(HTMLParser):
-    resault = 'None'
-    pwd = "/your/script/init/path"
-    dev_manufacturer = 'None'
-    dev_model = 'None'
-    dev_os_version = 'None'
+    xunit = 'NULL'
+    version = 'NULL'
+    include = 'NULL'
+    testcase = 'NULL'
+    outputdir= 'NULL'
+    input_cmd =''
+
+    dev_model = 'NULL'
+    dev_os_version = 'NULL'
+    dev_manufacturer = 'NULL'
+
+    resault = 'NULL'
+    pwd = "/your/script/workspace/path"
 
     def __init__(self):
         HTMLParser.__init__(self)
+
+    def usage(self):
+        print "user instroduction ;-)"
+
+    def get_version(self):
+        print "version 1.0.0"
 
     def get_pwd(self):
         return sys.path[0]
@@ -38,26 +53,26 @@ class MyParser(HTMLParser):
         print os.popen(cmd).read()
 
     def create_output_dir(self):
-        if  os.path.exists('./RFUI_outputs_dir'):
+        if os.path.exists('./RFUI_outputs_dir'):
             print ">>>RFUI_outputs_dir already exists"
         else:
             cmd = "mkdir RFUI_outputs_dir"
-            print '>>>'+os.popen(cmd).read()
+            print '>>>' + os.popen(cmd).read()
 
     def get_newest_apk(self):
         html = self.getHtml('http://10.240.129.99/nightly/')
-        self.feed(html)   # -->def handle_starttag():
+        self.feed(html)  # -->def handle_starttag():
         print "-------%s-------" % ctime()
         apk_version = self.resault
         apk_version = apk_version.replace('/', '')
 
-        fl=open('test_dev_info.properties','a')
-        fl.write('android_app_version='+apk_version+'\n')
+        fl = open('test_dev_info.properties', 'a')
+        fl.write('android_app_version=' + apk_version + '\n')
         fl.close()
 
-        if  os.path.exists('./'+apk_version+'.apk'):
+        if os.path.exists('./' + apk_version + '.apk'):
             print "the Apk already up-to-date\n"
-            sleep(8)               # wait appium start
+            sleep(8)  # wait appium start
         else:
             download_url = "http://10.240.129.99/nightly/" + apk_version + '/' + apk_version + ".apk"
             f = urllib2.urlopen(download_url)
@@ -65,10 +80,10 @@ class MyParser(HTMLParser):
             print self.pwd
             data = f.read()
             filename = apk_version + ".apk"
-            with open(filename, "wb") as code:                   #download newest apk
+            with open(filename, "wb") as code:  # download newest apk
                 code.write(data)
             print "Download Done!"
-            sleep(8)             # wait appium start
+            sleep(8)  # wait appium start
 
         # rm the old apk
         cmd = "rm ./YX_RFUI_Framework_demo/Resources/yixin_test.apk"
@@ -77,28 +92,27 @@ class MyParser(HTMLParser):
         # link newest apk to ./Res*/yixin_test.apk
         # Win:ln <src>  <des>  |  Mac/Linux: ln -s <src> <des>
         uname_str = platform.system()
-        os_arch =  re.search("Windows",uname_str)
+        os_arch = re.search("Windows", uname_str)
         if os_arch:
-            cmd = "ln  ./"    + apk_version + ".apk" + "   ./YX_RFUI_Framework_demo/Resources/yixin_test.apk"
+            cmd = "ln  ./" + apk_version + ".apk" + "   ./YX_RFUI_Framework_demo/Resources/yixin_test.apk"
             print os.popen(cmd).read()
         else:
             cmd = "ln -s ./" + apk_version + ".apk" + "   ./YX_RFUI_Framework_demo/Resources/yixin_test.apk"
             print os.popen(cmd).read()
 
-
     def get_device_info(self):
         cmd = "adb shell cat /system/build.prop"
         dev_info = os.popen(cmd).read()
         if dev_info:
-            self.dev_manufacturer = re.search(r"(ro.product.manufacturer=)(\S+)",dev_info).group(2)
-            self.dev_model = re.search(r"(ro.product.model=)(\S+)",dev_info).group(2)
-            self.dev_os_version= re.search(r"(ro.build.version.release=)(\S+)",dev_info).group(2)
-            print '>>>'+self.resault.replace('/','')
-            print '>>>'+self.dev_manufacturer,self.dev_model,self.dev_os_version
-            fl=open('test_dev_info.properties','a')
-            fl.write('android_dev_name='+self.dev_manufacturer+'\n')
-            fl.write('android_dev_model='+self.dev_model+'\n')
-            fl.write('android_version='+self.dev_os_version+'\n')
+            self.dev_manufacturer = re.search(r"(ro.product.manufacturer=)(\S+)", dev_info).group(2)
+            self.dev_model = re.search(r"(ro.product.model=)(\S+)", dev_info).group(2)
+            self.dev_os_version = re.search(r"(ro.build.version.release=)(\S+)", dev_info).group(2)
+            print '>>>' + self.resault.replace('/', '')
+            print '>>>' + self.dev_manufacturer, self.dev_model, self.dev_os_version
+            fl = open('test_dev_info.properties', 'a')
+            fl.write('android_dev_name=' + self.dev_manufacturer + '\n')
+            fl.write('android_dev_model=' + self.dev_model + '\n')
+            fl.write('android_version=' + self.dev_os_version + '\n')
             fl.close()
         else:
             print ">>>No device find!"
@@ -109,7 +123,19 @@ class MyParser(HTMLParser):
 
     def job_Mac(self):
         print "\n\n>>>here is from Mac\n\n"
+        cmd = ' '.join(self.input_cmd)
+        cmd = 'pybot'+' '+cmd
+        print os.popen(cmd).read()
 
+    def job_Windows(self):
+        print "\n\n>>>here is from Windows\n\n"
+        cmd = ' '.join(self.input_cmd)
+        cmd = 'C:\Python27\python -m robot.run'+' '+cmd
+        print os.popen(cmd).read()
+
+    def job_Linux(self):
+        print "[info]:Linux fx() need added in future ;-)"
+        """
         cmd = "pybot " \
               "--variable BROWSER:safari " \
               "--outputdir safari_dir" \
@@ -117,35 +143,50 @@ class MyParser(HTMLParser):
               "--xunit output_xunit.xml " \
               "--xunitskipnoncritical " \
               "./YX_RFUI_Framework_demo/Test/YX_Subscriptions/test_suite_examples.txt"
+        """
+        """
+        cmd = 'C:\Python27\python -m robot.run'+'  ' \
+              + '--include=' + self.include \
+              + '--xunit=' + self.xunit \
+              + '--outputdir=' + self.outputdir \
+              + self.testcase
         print os.popen(cmd).read()
-
-    def job_Windows(self):
-        print "\n\n>>>here is from Windows\n\n"
-
-        cmd = "C:\Python27\python -m robot.run " \
-              "--include=demo " \
-              "--xunit=xunitOutput.xml " \
-              "--outputdir=D:\JENKINS_hzqa_CI\workspace\yixin-WebUiTest-xdq\RFUI_outputs_dir " \
-              "D:\JENKINS_hzqa_CI\workspace\yixin-WebUiTest-xdq\YX_RFUI_Framework_demo\Test\YX_Subscriptions"
-        print os.popen(cmd).read()
-
-    def job_Linux(self):
-        print "[info]:Linux fx() need added in future ;-)"
+        """
 
 
 if __name__ == '__main__':
-
+    
     MyParser = MyParser()
-    MyParser.create_output_dir()     # mkdir ./RFUI_outputs_dir
-    MyParser.get_newest_apk()       # wget http://10.240.129.99/nightly/*_latest.apk
-    MyParser.get_device_info()       # adb shell cat /system/build.prop
-    MyParser.get_gitbucket()          # git clone *_demo
+    MyParser.input_cmd = sys.argv[1:]
+    """
+    opts,args = getopt.getopt(sys.argv[1:],"hvi:o:x:",["help","version","outputdir=","include=","xunit=","testcase="])
+    for op,value in opts:
+        if op == '-h' or op == '--help':
+            MyParser.usage()
+            sys.exit()
+        elif op == '-v' or op =='--version':
+            MyParser.get_version()
+            sys.exit()
+        elif op == '-i' or op == '--include':
+            MyParser.input_cmd += ' --inlcude='+value+' '
+        elif op == '-o' or op == '--outputdir':
+            MyParser.input_cmd += ' --outputdir='+value+' '
+        elif op == '-x' or op == '--xunit':
+            MyParser.input_cmd += ' --xunit'+value+'  '
+        elif op == '-t' or op == '--testcase':
+            MyParser.input_cmd += value+'  '
+    """
+
+    MyParser.create_output_dir()  # mkdir ./RFUI_outputs_dir
+    MyParser.get_newest_apk()  # wget http://10.240.129.99/nightly/*_latest.apk
+    MyParser.get_device_info()  # adb shell cat /system/build.prop
+    MyParser.get_gitbucket()  # git clone *_demo
 
     uname_str = platform.system()
     print uname_str
-    mac_arch = re.search("Darwin",uname_str)
-    win_arch = re.search("Windows",uname_str)
-    linux_arch = re.search("Linux",uname_str)
+    mac_arch = re.search("Darwin", uname_str)
+    win_arch = re.search("Windows", uname_str)
+    linux_arch = re.search("Linux", uname_str)
 
     if mac_arch:
         MyParser.job_Mac()
@@ -154,4 +195,4 @@ if __name__ == '__main__':
     elif linux_arch:
         MyParser.job_Linux()
     else:
-        print "[err]:can NOT detect OS type :-("
+        print "[x]:can NOT detect OS type :-("
